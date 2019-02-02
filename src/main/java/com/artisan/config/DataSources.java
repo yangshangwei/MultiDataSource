17e4ec9e1a1dd12b355c045117374b02fe4ac99c
yangshangwei <yangshangwei@inspur.com>
2019-02-03 00:38:22 +0800

    DataSources增加注释

--- src/main/java/com/artisan/config/DataSources.java
+++ src/main/java/com/artisan/config/DataSources.java
@@ -6,13 +6,13 @@ package com.artisan.config;
  *
  */
 public interface DataSources {
-	
+	// 数据库
 	String DB1 = "db1";
     String DB2 = "db2";
-    
+    // JdbcTemplate
     String DB1_JDBCTEMPLATE = "db1JdbcTemplate";
     String DB2_JDBCTEMPLATE = "db2JdbcTemplate";
-    
+    // 事务管理
     String DB1_TRANSACTION = "db1TransactionManager";
     String DB2_TRANSACTION = "db2TransactionManager";
 }
